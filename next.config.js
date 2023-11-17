4eff08ec6b9c142fdd2d443549bcb47f1891acf4
Robert Kirakosyan <kirakosyanrobert2018@gmail.com>
2023-11-17 21:06:57 +0400

    added gym details page

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  reactStrictMode: true,
   swcMinify: true,
   modularizeImports: {
     '@mui/icons-material': {
