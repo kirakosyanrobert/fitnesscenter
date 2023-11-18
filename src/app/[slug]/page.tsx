53f5a033ea9922c1bb336e7e1800a247d5930f86
Robert Kirakosyan <kirakosyanrobert2018@gmail.com>
2023-11-18 20:57:21 +0400

    added generateStaticPaths

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -2,6 +2,10 @@ import { Container, Grid } from '@mui/material';
 import MediaCard from '@/components/MediaCard';
 import { Gym, gyms } from '@/mock/gyms';
 
+export function generateStaticParams() {
+  return gyms.map(g => ({ slug: g.instagramNick }));
+}
+ 
 export default function GymPage({ params }: { params: { slug: string } }) {
   console.log(params.slug);
   const gym = gyms.find(g => g.instagramNick === params.slug) as Gym;
